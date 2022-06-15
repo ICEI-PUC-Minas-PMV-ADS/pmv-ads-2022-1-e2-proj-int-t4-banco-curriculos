a615b6958d33ac2714158f370032c8866a7c4d54
sabrinachs <sabrinach08@yahoo.com.br>
2022-06-14 21:00:53 -0300

    Mudança para o banco no Azure

--- Prova-POC/jobs-web/Migrations/20220614235604_mudanca-tipo-dados.Designer.cs
+++ Prova-POC/jobs-web/Migrations/20220614235604_mudanca-tipo-dados.Designer.cs
@@ -4,14 +4,16 @@ using Jobs.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Jobs.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220614235604_mudanca-tipo-dados")]
+    partial class mudancatipodados
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Prova-POC/jobs-web/Migrations/20220614235604_mudanca-tipo-dados.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Jobs.Migrations
+{
+    public partial class mudancatipodados : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "contato",
+                table: "Empresa",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "nome",
+                table: "Empresa",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Race",
+                table: "Candidate",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Pwd",
+                table: "Candidate",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Candidate",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "contato",
+                table: "Empresa");
+
+            migrationBuilder.DropColumn(
+                name: "nome",
+                table: "Empresa");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Race",
+                table: "Candidate",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Pwd",
+                table: "Candidate",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Gender",
+                table: "Candidate",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
