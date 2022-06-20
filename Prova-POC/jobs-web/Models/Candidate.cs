9553503236be1cf2dd9557c6f43340c40a0c469b
Isabella Fernanda Campos Emiliano <isabellaemiliano14@gmail.com>
2022-06-20 19:16:45 -0300

    corrigindo erro da home

--- Prova-POC/jobs-web/Models/Candidate.cs
+++ Prova-POC/jobs-web/Models/Candidate.cs
@@ -13,7 +13,7 @@ namespace Jobs.Models
         [Key]
         public int Id { get; set; }
 
-        
+
         [Required(ErrorMessage = "Por favor, preencha o campo")]
         [Display(Name = "Data de Nascimento")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
@@ -22,27 +22,21 @@ namespace Jobs.Models
 
         [Display(Name = "Gênero")]
         [Required(ErrorMessage = "Por favor, preencha o campo")]
-        public Gender Gender { get; set; }
+        public string Gender { get; set; }
 
         [Display(Name = "Raça")]
         [Required(ErrorMessage = "Por favor, preencha o campo")]
-        public Race Race { get; set; }
+        public string Race { get; set; }
 
         [Display(Name = "PCD")]
         [Required(ErrorMessage = "Por favor, preencha o campo")]
-        public Pwd Pwd { get; set; }
+        public string Pwd { get; set; }
 
         [Display(Name = "CPF")]
         [Required(ErrorMessage = "Por favor, preencha o campo")]
         public string Cpf { get; set; }
 
         public string Linkedin { get; set; }
-
-       // [Display(Name="Candidato")]
-        //public int CandidateId { get; set; }
-
-       // [ForeignKey("UserId")]
-       // public virtual User User { get; set; }
 
         [ForeignKey("User")]
         public int UserId { get; set; }
@@ -52,28 +46,4 @@ namespace Jobs.Models
         public ICollection<Education> Educations { get; set; }
     }
 
-    public enum Gender
-    {
-        Feminino,
-        Masculino,
-        [Display(Name = "Prefiro não dizer")]
-        Nao
-    }
-
-    public enum Pwd
-    {
-        Sim,
-        [Display(Name = "Não")]
-        Nao
-    }
-
-    public enum Race
-    {
-        Branca,
-        Preta,
-        Parda,
-        Amarela,
-        [Display(Name = "Indígena")]
-        Indigena
-    }
 }
